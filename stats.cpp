30a9bd7dd501e915065cb56644a25d39ba05fb53
Anila Anumolu <Anila.Anumolu@philips.com>
2024-08-30 06:21:05 -0700

    Assignment1

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,26 @@
 #include "stats.h"
+#include <cmath>
+ 
+using namespace std;
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+Stats Statistics::ComputeStatistics(const std::vector<float>& ) {
+    
+	Statistics::Stats st;
+    st.average = NAN;
+    st.max = NAN;
+    st.min = NAN;
+    float total = 0;
+    int arr_size = arr.size();
+    if(arr.size()){
+ 
+        for(auto i : arr){
+            total += i;
+            st.max = (st.max > i) ? st.max : i ;
+            st.min = (st.min < i) ? st.min : i ;
+        }
+ 
+        st.average = total/arr_size;
+    }
+ 
+    return st;
 }
